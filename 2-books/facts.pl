af5b4ebe31dc290bd6cd0021f24ddc768d1cafab
kalaider <a.kalaider@yandex.ru>
2018-02-19 20:16:03 +0300

    feat(book): add a number of facts and some docs

---
+++ 2-books/facts.pl
@@ -0,0 +1,18 @@
+﻿/**
+ * facts:
+ *
+ *     book((Name, Price, Count))
+ */
+
+book(("Effective Java: Programming Language Guide", 100, 10)).
+book(("Patterns of Enterprise Application Architecture", 300, 20)).
+book(("Domain-Driven Design. Tackling Complexity in the Heart of Software", 150, 8)).
+book(("Thinking In Java", 100, 6)).
+book(("The C++ Programming Language", 300, 50)).
+book(("Java Concurrency in Practice", 250, 10)).
+book(("JSR-288", 0, 1)).
+book(("The Art of Computer Programming Vol 1", 110, 5)).
+book(("The Art of Computer Programming Vol 2", 120, 7)).
+book(("The Art of Computer Programming Vol 3", 130, 9)).
+book(("The Art of Computer Programming Vol 4", 140, 11)).
+book(("Introduction to Algorithms", 180, 12)).
